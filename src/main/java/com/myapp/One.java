0a8922c08382bdb142b69eca9ee18c6417047b7e
Bhargav Mekapothula <mail2bhargavm@gmail.com>
2022-07-20 23:27:11 +0100

    Generated Jar file

---
+++ src/main/java/com/myapp/One.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class One 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from one java file" );
+    }
+}
